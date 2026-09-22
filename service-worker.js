4adc9d4e863df7193a0e2089ae9d3682cd417f9d
Tzcreation <tzcreation2018@gmail.com>
2026-09-22 21:31:54 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='skin-routine-v19';
+const CORE=['./','./index.html','./style.css?v=19','./app.js?v=19','./manifest.json?v=19','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
